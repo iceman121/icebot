d0b560562f540a33ec489ea6038d2950203e5083
iceman121 <shashwatpat@gmail.com>
2021-11-10 19:58:08 -0800

    Adding ip query command

--- icebot.py
+++ icebot.py
@@ -1,8 +1,11 @@
-#!usr/bin/env python3
+#!/usr/bin python3
 
 import discord
 from discord.ext import commands
+
 from configparser import ConfigParser
+
+import requests
 
 parser = ConfigParser()
 parser.read('cfg.ini')
@@ -13,5 +16,12 @@ bot = commands.Bot(command_prefix='>')
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+@bot.command()
+async def ip(ctx):
+    ip_query = requests.request('GET', 'https://icanhazip.com').replace('\\n', '')
+    await ctx.send(ip_query.text)
+
 
 bot.run(parser['Discord']['token'])
