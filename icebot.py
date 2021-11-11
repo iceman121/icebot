fc27be11754881384ee8865f945b78c6bfb2cf95
iceman121 <shashwatpat@gmail.com>
2021-11-10 20:26:24 -0800

    cpu and ram util

--- icebot.py
+++ icebot.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from configparser import ConfigParser
 
 import requests
+import psutil
 
 parser = ConfigParser()
 parser.read('cfg.ini')
@@ -22,6 +23,13 @@ async def ping(ctx):
 async def ip(ctx):
     ip_query = requests.request('GET', 'https://icanhazip.com')
     await ctx.send(ip_query.text.replace('\\n', ''))
+
+
+@bot.command()
+async def status(ctx):
+    cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory().percent
+    await ctx.send(f'CPU util = {cpu:.2f}%, RAM util = {ram:.2f}%')
 
 
 bot.run(parser['Discord']['token'])
